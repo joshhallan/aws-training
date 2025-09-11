66c2b1d3c1046ca16c09f36e28843898c6759af7
Josh Allan <joshuaallan@live.co.uk>
2025-09-11 17:34:42 +0100

    Module 6 complete - edited create customer and required

---
+++ leighton-crm/src/functions/create-customer/create-customer-schema.ts
@@ -0,0 +1,60 @@
+export const schema = {
+  $schema: 'http://json-schema.org/draft-07/schema#',
+  title: 'Customer',
+  type: 'object',
+  properties: {
+    // customerId: {
+    //   type: 'string',
+    // },
+    firstName: {
+      type: 'string',
+    },
+    lastName: {
+      type: 'string',
+    },
+    email: {
+      type: 'string',
+      format: 'email',
+    },
+    phone: {
+      type: 'string',
+    },
+    company: {
+      type: 'string',
+    },
+    jobTitle: {
+      type: 'string',
+    },
+    address: {
+      type: 'object',
+      properties: {
+        street: {
+          type: 'string',
+        },
+        city: {
+          type: 'string',
+        },
+        state: {
+          type: 'string',
+        },
+        postalCode: {
+          type: 'string',
+        },
+        country: {
+          type: 'string',
+        },
+      },
+      required: ['street', 'city', 'state', 'postalCode', 'country'],
+    },
+  },
+  required: [
+    // 'customerId',
+    'firstName',
+    'lastName',
+    'email',
+    'phone',
+    'company',
+    'jobTitle',
+    'address',
+  ],
+};
--- leighton-crm/src/functions/create-customer/create-customer.ts
+++ leighton-crm/src/functions/create-customer/create-customer.ts
@@ -1,21 +1,41 @@
+import { injectLambdaContext } from '@aws-lambda-powertools/logger/middleware';
+import { MetricUnit, Metrics } from '@aws-lambda-powertools/metrics';
+import { logMetrics } from '@aws-lambda-powertools/metrics/middleware';
+import { Tracer } from '@aws-lambda-powertools/tracer';
+import { captureLambdaHandler } from '@aws-lambda-powertools/tracer/middleware';
 import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
 import { marshall } from '@aws-sdk/util-dynamodb';
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
 import type { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { randomUUID } from 'node:crypto';
 import { CreateCustomer } from '../../dto/create-customer/create-customer';
 import { Customer } from '../../dto/customer/customer';
+import { ValidationError } from '../../errors/validation-error';
+import { errorHandler } from '../../shared/error-handler/error-handler';
+import { logger } from '../../shared/logger/logger';
+import { schemaValidator } from '../../shared/schema-validator/schema-validator';
+import { schema } from './create-customer-schema';
+
+const tracer = new Tracer();
+const metrics = new Metrics({
+  serviceName: 'leighton-hr',
+  namespace: 'leighton-hr',
+});
 
 const client = new DynamoDBClient();
 
-export const handler = async (
+export const createCustomerHandler = async (
   event: APIGatewayProxyEvent,
 ): Promise<APIGatewayProxyResult> => {
   try {
     if (!event.body) {
-      throw new Error('No payload body');
+      throw new ValidationError('No payload body');
     }
 
     const newCustomer = JSON.parse(event.body) as CreateCustomer;
+
+    schemaValidator(schema, newCustomer);
 
     const id = randomUUID();
     const currentDate = new Date().toISOString();
@@ -39,21 +59,27 @@ export const handler = async (
 
     await client.send(putCommand);
 
+    logger.info(`Customer ${newCustomer.customerId} created.`);
+
+    metrics.addMetric('SuccessfulCreateCustomer', MetricUnit.Count, 1);
+
     return {
       statusCode: 200,
       body: JSON.stringify(newCustomer),
     };
   } catch (error) {
     let errorMessage = 'Unknown error';
-    if (error instanceof Error) {
-      errorMessage = error.message;
-    }
+    if (error instanceof Error) errorMessage = error.message;
+    logger.error(errorMessage);
 
-    console.error(error);
+    metrics.addMetric('CreateCustomerError', MetricUnit.Count, 1);
 
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: errorMessage }),
-    };
+    return errorHandler(error);
   }
 };
+
+export const handler = middy(createCustomerHandler)
+  .use(injectLambdaContext(logger))
+  .use(captureLambdaHandler(tracer))
+  .use(logMetrics(metrics))
+  .use(httpErrorHandler());
